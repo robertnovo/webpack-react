9566c445982b3a169cd2a74efb5acb42d545f318
Robert Novo <robert.j.novo@gmail.com>
2019-11-25 22:05:42 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import 'core-js'
+import './styles.css'
+import App from './App'
+
+ReactDOM.render(<App />, document.getElementById('app'))

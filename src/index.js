62a4077f1d7c812bb1964edd04fd34b7cb559a01
Robert Novo <robert.j.novo@gmail.com>
2019-11-25 22:34:30 +0100

    added jest

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import 'core-js'
-import './styles.css'
-import App from './App'
+import React from 'react';
+import { render } from 'react-dom';
+import 'core-js';
+import './styles.css';
+import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('app'))
+render(<App />, document.getElementById('app'));

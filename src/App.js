b563605a383a6b7ecd9efd5c6df153a6f18a9137
Robert Novo <robert.j.novo@gmail.com>
2019-11-25 23:48:28 +0100

    trying out Suspense

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, { useState, lazy, Suspense } from 'react';
+
+const Warning = lazy(() => import('./Warning'));
 
 function App() {
-  return <div>Hello world</div>;
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <h1>Hello world</h1>
+      {count > 3 && (
+        <Suspense fallback={null}>
+          <Warning />
+        </Suspense>
+      )}
+      <button onClick={() => setCount(count + 1)}>{count}</button>
+    </div>
+  );
 }
 
 export default App;
